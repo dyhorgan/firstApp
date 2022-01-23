e04d19add60eda9c527fc1263f57e0701399c23d
Dylan Horgan <dyhorgan@gmail.com>
2022-01-23 00:58:48 -0500

    committing change

---
+++ app/src/main/java/com/example/numad22sp_dylanhorgan/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.numad22sp_dylanhorgan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button aboutMe = findViewById(R.id.aboutMe);
+
+        aboutMe.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            Log.i("tag", "randomMessageThin");
+            Toast.makeText(getApplicationContext(), "Dylan Horgan - dyhorgan@gmail.com", Toast.LENGTH_SHORT).show();
+          }
+        });
+
+    }
+}

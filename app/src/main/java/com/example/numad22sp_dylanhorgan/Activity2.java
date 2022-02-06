6bda20aeadca6767dbd32b93659ccbe05ef010a6
Dylan Horgan <dyhorgan@gmail.com>
2022-02-05 19:20:47 -0500

    everything seems to be working, implemented rotation listener

--- app/src/main/java/com/example/numad22sp_dylanhorgan/Activity2.java
+++ app/src/main/java/com/example/numad22sp_dylanhorgan/Activity2.java
@@ -1,7 +1,9 @@
 package com.example.numad22sp_dylanhorgan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,11 +12,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
-    static void clickFunc(TextView t, Button b){
-      String s = "Pressed: "  +  b.getText();
+    static void clickFunc(TextView t, Button b) {
+      String s = "Pressed: " + b.getText();
+      Log.i("tag", "ClickMessageThing");
       t.setText(s);
     }
-    @Override
+  TextView textButton = null;
+
+  static void rotateFunc(TextView t){
+    String s = "Pressed: A";
+    t.setText(s);
+  }
+  @Override
+  public void onConfigurationChanged(@NonNull Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Log.i("tag", "RotationMessageThing");
+    rotateFunc(this.textButton);
+  }
+
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
@@ -26,9 +42,11 @@ public class Activity2 extends AppCompatActivity {
         Button E = findViewById(R.id.E);
         Button F = findViewById(R.id.F);
         TextView pressed = findViewById(R.id.pressed);
+        this.textButton = pressed;
         A.setOnClickListener(new View.OnClickListener(){
           @Override
           public void onClick(View view) {
+
                   clickFunc(pressed, A);
           }
         });
